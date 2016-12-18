187a8e334e0832b1f988d8c56fa7b57aad0d910b
eduard-paul <eduard.paul.94@gmail.com>
2016-12-18 13:29:27 +0300

    Database stub

---
+++ Server/DataBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class DataBase : IDatabase
+    {
+        public void put(DeliveryPizzaLib.Manager.Order order)
+        {
+            // stub
+        }
+
+        public List<DeliveryPizzaLib.Manager.Order> getSince(DateTime time)
+        {
+            // stub
+            return new List<DeliveryPizzaLib.Manager.Order>();
+        }
+
+        public List<DeliveryPizzaLib.Manager.Order> getBetween(DateTime start, DateTime end)
+        {
+            // stub
+            return new List<DeliveryPizzaLib.Manager.Order>();
+        }
+
+        public List<DeliveryPizzaLib.Manager.Order> getByDriverId(int id)
+        {
+            // stub
+            return new List<DeliveryPizzaLib.Manager.Order>();
+        }
+
+        public List<DeliveryPizzaLib.Manager.Order> getByBranchId(int id)
+        {
+            // stub
+            return new List<DeliveryPizzaLib.Manager.Order>();
+        }
+
+        public List<DeliveryPizzaLib.Manager.Order> getByPizzaType(int id)
+        {
+            // stub
+            return new List<DeliveryPizzaLib.Manager.Order>();
+        }
+    }
+}
